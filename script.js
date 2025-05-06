c0644bbc5b71cd724b89ad77e123de423dfbf1ef
zaki <zaki-munshi@hotmail.co.uk>
2025-05-06 21:16:27 +0100

    Add for loop to not make it hard-coded
    Adding for loop allows the input to be created based
    on the number selected in the dropdown

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ const container = document.querySelector('.container')
 
 btn.addEventListener('click', function () {
      const playerOptions = selector.value
-    
 
-    if (Number(playerOptions) === 5) {
+     for (let i = 0; i < playerOptions; i++) {
         const playerInput = document.createElement('input')
         container.appendChild(playerInput)
-    }
-})
+     }
+    
 
-// if user selects 5-aside display 10 input fields
+})
