3784d2be67083e4fe56fbd2bd41d898c10df52e4
zaki <zaki-munshi@hotmail.co.uk>
2025-05-06 21:22:57 +0100

    Create h3 element add to div when btn clicked
    This will show which is team 1 and team 2

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const container = document.querySelector('.container')
 
 btn.addEventListener('click', function () {
      const playerOptions = selector.value
+     const h3 = document.createElement('h3')
+     const teamOne = document.createTextNode('Team 1')
+     container.appendChild(teamOne)
 
      for (let i = 0; i < playerOptions; i++) {
         const playerInput = document.createElement('input')
