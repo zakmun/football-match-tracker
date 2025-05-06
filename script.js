946d24696c0f93851e9ee0aaaf9e57b62b41ede3
zaki <zaki-munshi@hotmail.co.uk>
2025-05-06 20:53:56 +0100

    Create input element to display when btn clicked
    Calling the container will allow the input field
    to be appended inside the container if selector = 5

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const btn = document.querySelector('.btn')
 const selector = document.querySelector('#players')
+const container = document.querySelector('.container')
 
 btn.addEventListener('click', function () {
-    const playerOptions = selector.value
-    console.log(playerOptions)
+     const playerOptions = selector.value
+    
+
+    if (Number(playerOptions) === 5) {
+        const playerInput = document.createElement('input')
+        container.appendChild(playerInput)
+    }
 })
+
+// if user selects 5-aside display 10 input fields
