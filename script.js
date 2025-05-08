c01a56e6b3dc3e0f1950c5705fd5e20c851fa0e1
zaki <zaki-munshi@hotmail.co.uk>
2025-05-08 20:45:24 +0100

    Allow new inputs to display if t1 & t2 are diff
    If the user clicks another player option thats different
    to t1 and t2 then allow the code to run again for the new
    input fields

--- script.js
+++ script.js
@@ -7,57 +7,63 @@ Selectbtn.addEventListener('click', function () {
 
      if ((!document.querySelector('.teamOneContainer')) && (!document.querySelector('.teamTwoContainer')) ) {
 
-        const h3teamOne = document.createElement('h3')
-     const teamOne = document.createTextNode('Team 1')
-     h3teamOne.appendChild(teamOne)
-     container.appendChild(h3teamOne)
+        if (playerOptions !== teamOneInput || playerOptions !== teamTwoInput) {
 
-     for (let i = 0; i < playerOptions; i++) {
-        const teamOneContainer = document.createElement('div')
-        teamOneContainer.classList.add('teamOneContainer')
-        const teamOneInput = document.createElement('input')
-        teamOneInput.required
-        teamOneContainer.appendChild(teamOneInput)
-        container.appendChild(teamOneContainer)
-
-     }
-
-     if (!document.querySelector('.teamTwoContainer')) {
-        const h3TeamTwo = document.createElement('h3')
-        const teamTwo = document.createTextNode('Team Two')
-        h3TeamTwo.appendChild(teamTwo)
-        container.appendChild(h3TeamTwo)
+            const h3teamOne = document.createElement('h3')
+            const teamOne = document.createTextNode('Team 1')
+            h3teamOne.appendChild(teamOne)
+            container.appendChild(h3teamOne)
     
-     }
-
-     for (let i = 0; i < playerOptions; i++){
-        const teamTwoContainer = document.createElement('div')
-        teamTwoContainer.classList.add('teamTwoContainer')
-        const teamTwoInput = document.createElement('input')
-        teamTwoInput.required
-        teamTwoContainer.appendChild(teamTwoInput)
-        container.appendChild(teamTwoContainer)
-
-     }
-
-     if ((!document.querySelector('.submitBtn'))) {
-
-        if ((document.querySelector('.teamOneContainer')) && (document.querySelector('.teamTwoContainer')) ) {
-            const submitBtn = document.createElement('button')
-            submitBtn.classList.add('submitBtn')
-            const submitBtnTxt = document.createTextNode('Submit') 
-            submitBtn.appendChild(submitBtnTxt)
-            container.appendChild(submitBtn)
+         for (let i = 0; i < playerOptions; i++) {
+            const teamOneContainer = document.createElement('div')
+            teamOneContainer.classList.add('teamOneContainer')
+            const teamOneInput = document.createElement('input')
+            teamOneInput.required
+            teamOneContainer.appendChild(teamOneInput)
+            container.appendChild(teamOneContainer)
+    
          }
-
-     }
-     }
-     
     
+         if (!document.querySelector('.teamTwoContainer')) {
+            const h3TeamTwo = document.createElement('h3')
+            const teamTwo = document.createTextNode('Team Two')
+            h3TeamTwo.appendChild(teamTwo)
+            container.appendChild(h3TeamTwo)
+        
+         }
+    
+         for (let i = 0; i < playerOptions; i++){
+            const teamTwoContainer = document.createElement('div')
+            teamTwoContainer.classList.add('teamTwoContainer')
+            const teamTwoInput = document.createElement('input')
+            teamTwoInput.required
+            teamTwoContainer.appendChild(teamTwoInput)
+            container.appendChild(teamTwoContainer)
+    
+         }
+    
+         if ((!document.querySelector('.submitBtn'))) {
+    
+            if ((document.querySelector('.teamOneContainer')) && (document.querySelector('.teamTwoContainer')) ) {
+                const submitBtn = document.createElement('button')
+                submitBtn.classList.add('submitBtn')
+                const submitBtnTxt = document.createTextNode('Submit') 
+                submitBtn.appendChild(submitBtnTxt)
+                container.appendChild(submitBtn)
+             }
+    
+         }
+         }
+         
+
+        }
+
+   
      
 
 
 })
 
-// if both team containers are inside of main container and user presses select button again then 
+// if user selects another playeroption while container is full with team1 and team2 then run the code
+// if playersOptions !== team1input or team2input then
 
